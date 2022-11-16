ca63c44d8cf2c7f1154355282ac642e84e9a6184
rishiraj007 <rishiraj@mounttalent.com>
2022-11-16 18:49:41 +0530

    [Prabhu] Add: Find Max Number Added

---
+++ FindMaximumNumber/FindMaxGeneric.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumNumber
+{
+    internal class FindMaxGeneric 
+    {
+        public T FindMax<T>(T a,T b,T c) where T : IComparable
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                return a;
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                return b;
+            else
+                return c;
+        }
+    }
+}
---
+++ FindMaximumNumber/FindMaxNum.cs
@@ -0,0 +1,33 @@
+﻿namespace FindMaximumNumber
+{
+    public class FindMaxNum
+    {
+        public int FindMaxInteger(int a, int b, int c)
+        {
+            if (a > b && a > c)
+                return a;
+            if (b > a && b > c)
+                return b;
+            else
+                return c;
+        }
+        public float FindMaxFloat(float a, float b, float c)
+        {
+            if (a > b && a > c)
+                return a;
+            if (b > a && b > c)
+                return b;
+            else
+                return c;
+        }
+        public string FindMaxString(string a, string b, string c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0)
+                return a;
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0)
+                return b;
+            else
+                return c;
+        }
+    }
+}
